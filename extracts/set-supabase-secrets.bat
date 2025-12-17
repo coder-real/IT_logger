420824a582ca16154ba5523dd86ef8282c1150f1
Anthony <nderanthony60@gmail.com>
2025-12-18 00:06:04 +0100

    Cleanup project and configure for GitHub Pages deployment

--- extracts/set-supabase-secrets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting Supabase secrets...
-supabase secrets set FACEPP_API_KEY=xGhIJ0cABz-hh3okt1N0vGjUItdwnvLk
-supabase secrets set FACEPP_API_SECRET=KFzkTcIwKGdtO8NgzhNkaeDOVZURsA56
-supabase secrets set FACEPP_FACESET_TOKEN=f7a4a95bbc987c37964d09ec81e725b5
-echo Done!
-pause
